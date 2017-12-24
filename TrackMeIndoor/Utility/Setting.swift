f78f99aba9d6dece766ecb0dd2f88dcd952acc94
Sam <neiamenase@gmail.com>
2017-12-24 13:43:01 +0800

    Complete Setting

--- TrackMeIndoor/Utility/Setting.swift
+++ TrackMeIndoor/Utility/Setting.swift
@@ -1,17 +1,50 @@
 
 import UIKit
 
-class Setting: NSObject, NSCoding {
-    static let u = 2.0
-    static let v = 2.0
+class SaveSetting: NSObject, NSCoding {
+
+    //MARK: Properties
+    var u: Double
+    var v: Double
+    
+    //MARK: Types
+    init?(u: Double, v: Double) {
+        
+        // The name must not be empty
+        guard !u.isZero else {
+            return nil
+        }
+        
+        // The rating must be between 0 and 5 inclusively
+        guard !v.isZero else {
+            return nil
+        }
+        
+        // Initialize stored properties.
+        self.u = Double(u)
+        self.v = Double(v)
+        
+    }
+
+    
     func encode(with aCoder: NSCoder) {
-        <#code#>
+        aCoder.encode(u, forKey: Setting.u)
+        aCoder.encode(v, forKey: Setting.v)
     }
     
-    required init?(coder aDecoder: NSCoder) {
-        <#code#>
+    struct Setting {
+        static let u = "2"
+        static let v = "2"
+    }
+    
+    required convenience init?(coder aDecoder: NSCoder) {
+        let u = aDecoder.decodeDouble(forKey: Setting.u)
+        let v = aDecoder.decodeDouble(forKey: Setting.v)
+        self.init(u: u, v: v)
     }
     
 
     
 }
+
+
